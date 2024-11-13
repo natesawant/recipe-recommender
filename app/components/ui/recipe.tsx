d0aa3bc3c21ab24aa7260a5540e3274de16ab624
Nate Sawant <natesawant@gmail.com>
2024-11-13 14:24:03 -0500

    Add Collapsible ingredients/directions

--- app/components/ui/recipe.tsx
+++ app/components/ui/recipe.tsx
@@ -1,5 +1,6 @@
 import { Recipe } from "@/app/_text";
 import { CardDescription, CardTitle } from "./card";
+import Collapsible from 'react-collapsible';
 
 export default function RecipeCard(props: { recipe: Recipe }) {
     const name = props.recipe.name;
@@ -7,18 +8,17 @@ export default function RecipeCard(props: { recipe: Recipe }) {
     const ingredients = props.recipe.ingredients
     const directions = props.recipe.directions
 
+
     return <li className="flex flex-col gap-2">
         <div>
             <CardTitle>{name}</CardTitle>
             <CardDescription className="italic">{description}</CardDescription>
         </div>
-        <div>
-            <h3>Ingredients</h3>
+        <Collapsible trigger="Ingredients" easing="ease-in-out">
             <ol className="list-disc list-inside">{ingredients.map((ing, iidx) => <li key={iidx}>{ing}</li>)}</ol>
-        </div>
-        <div>
-            <h3>Directions</h3>
+        </Collapsible >
+        <Collapsible trigger="Directions" easing="ease-in-out">
             <ol className="list-decimal list-inside">{directions.map((dir, didx) => <li key={didx}>{dir}</li>)}</ol>
-        </div>
-    </li>
+        </Collapsible>
+    </li >
 }
